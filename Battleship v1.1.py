8eac7ed131cf5c6c71714a02f63d4b2b4aad3299
ChrAshPun <punlac1222@gmail.com>
2019-08-02 20:15:43 -0700

    First commit

--- Battleship v1.1.py
+++ Battleship v1.1.py
@@ -1,174 +1,57 @@
-def display_board(a,b):
-    
-    if place_largeship:
-    
-        print("    A   B   C   D   E")
-        print("   ------------------- ")  
-        print("5 | {} | {} | {} | {} | {} |".format(b[1][5],b[2][5],b[3][5],b[4][5],b[5][5]))
-        print("  |-------------------|")
-        print("4 | {} | {} | {} | {} | {} |".format(b[1][4],b[2][4],b[3][4],b[4][4],b[5][4]))
-        print("  |-------------------|")
-        print("3 | {} | {} | {} | {} | {} |".format(b[1][3],b[2][3],b[3][3],b[4][3],b[5][3]))
-        print("  |-------------------|")
-        print("2 | {} | {} | {} | {} | {} |".format(b[1][2],b[2][2],b[3][2],b[4][2],b[5][2]))
-        print("  |-------------------|")
-        print("1 | {} | {} | {} | {} | {} |".format(b[1][1],b[2][1],b[3][1],b[4][1],b[5][1]))
-        print("  |-------------------|")
-        print("  |||||||||||||||||||||")
-        print("  |-------------------|")  
-        print("5 | {} | {} | {} | {} | {} |     Large Battleship".format(a[1][5],a[2][5],a[3][5],a[4][5],a[5][5]))
-        print("  |-------------------|     O O O")
-        print("4 | {} | {} | {} | {} | {} |     Medium Battleship 1".format(a[1][4],a[2][4],a[3][4],a[4][4],a[5][4]))
-        print("  |-------------------|     O O")
-        print("3 | {} | {} | {} | {} | {} |     Medium Battleship 2".format(a[1][3],a[2][3],a[3][3],a[4][3],a[5][3]))
-        print("  |-------------------|     O O")
-        print("2 | {} | {} | {} | {} | {} |     Small Battleship".format(a[1][2],a[2][2],a[3][2],a[4][2],a[5][2]))
-        print("  |-------------------|     O")
-        print("1 | {} | {} | {} | {} | {} |".format(a[1][1],a[2][1],a[3][1],a[4][1],a[5][1]))
-        print("   ------------------- ")
-        print("    A   B   C   D   E")
-    elif place_mediumship1:
-        print("    A   B   C   D   E")
-        print("   ------------------- ")  
-        print("5 | {} | {} | {} | {} | {} |".format(b[1][5],b[2][5],b[3][5],b[4][5],b[5][5]))
-        print("  |-------------------|")
-        print("4 | {} | {} | {} | {} | {} |".format(b[1][4],b[2][4],b[3][4],b[4][4],b[5][4]))
-        print("  |-------------------|")
-        print("3 | {} | {} | {} | {} | {} |".format(b[1][3],b[2][3],b[3][3],b[4][3],b[5][3]))
-        print("  |-------------------|")
-        print("2 | {} | {} | {} | {} | {} |".format(b[1][2],b[2][2],b[3][2],b[4][2],b[5][2]))
-        print("  |-------------------|")
-        print("1 | {} | {} | {} | {} | {} |".format(b[1][1],b[2][1],b[3][1],b[4][1],b[5][1]))
-        print("  |-------------------|")
-        print("  |||||||||||||||||||||")
-        print("  |-------------------|")  
-        print("5 | {} | {} | {} | {} | {} |     ".format(a[1][5],a[2][5],a[3][5],a[4][5],a[5][5]))
-        print("  |-------------------|          ")
-        print("4 | {} | {} | {} | {} | {} |     Medium Battleship 1".format(a[1][4],a[2][4],a[3][4],a[4][4],a[5][4]))
-        print("  |-------------------|     O O")
-        print("3 | {} | {} | {} | {} | {} |     Medium Battleship 2".format(a[1][3],a[2][3],a[3][3],a[4][3],a[5][3]))
-        print("  |-------------------|     O O")
-        print("2 | {} | {} | {} | {} | {} |     Small Battleship".format(a[1][2],a[2][2],a[3][2],a[4][2],a[5][2]))
-        print("  |-------------------|     O")
-        print("1 | {} | {} | {} | {} | {} |".format(a[1][1],a[2][1],a[3][1],a[4][1],a[5][1]))
-        print("   ------------------- ")
-        print("    A   B   C   D   E")
-    elif place_mediumship2:
-        print("    A   B   C   D   E")
-        print("   ------------------- ")  
-        print("5 | {} | {} | {} | {} | {} |".format(b[1][5],b[2][5],b[3][5],b[4][5],b[5][5]))
-        print("  |-------------------|")
-        print("4 | {} | {} | {} | {} | {} |".format(b[1][4],b[2][4],b[3][4],b[4][4],b[5][4]))
-        print("  |-------------------|")
-        print("3 | {} | {} | {} | {} | {} |".format(b[1][3],b[2][3],b[3][3],b[4][3],b[5][3]))
-        print("  |-------------------|")
-        print("2 | {} | {} | {} | {} | {} |".format(b[1][2],b[2][2],b[3][2],b[4][2],b[5][2]))
-        print("  |-------------------|")
-        print("1 | {} | {} | {} | {} | {} |".format(b[1][1],b[2][1],b[3][1],b[4][1],b[5][1]))
-        print("  |-------------------|")
-        print("  |||||||||||||||||||||")
-        print("  |-------------------|")  
-        print("5 | {} | {} | {} | {} | {} |     ".format(a[1][5],a[2][5],a[3][5],a[4][5],a[5][5]))
-        print("  |-------------------|          ")
-        print("4 | {} | {} | {} | {} | {} |     ".format(a[1][4],a[2][4],a[3][4],a[4][4],a[5][4]))
-        print("  |-------------------|          ")
-        print("3 | {} | {} | {} | {} | {} |     Medium Battleship 2".format(a[1][3],a[2][3],a[3][3],a[4][3],a[5][3]))
-        print("  |-------------------|     O O")
-        print("2 | {} | {} | {} | {} | {} |     Small Battleship".format(a[1][2],a[2][2],a[3][2],a[4][2],a[5][2]))
-        print("  |-------------------|     O")
-        print("1 | {} | {} | {} | {} | {} |".format(a[1][1],a[2][1],a[3][1],a[4][1],a[5][1]))
-        print("   ------------------- ")
-        print("    A   B   C   D   E")
-    elif place_smallship:
-        print("    A   B   C   D   E")
-        print("   ------------------- ")  
-        print("5 | {} | {} | {} | {} | {} |".format(b[1][5],b[2][5],b[3][5],b[4][5],b[5][5]))
-        print("  |-------------------|")
-        print("4 | {} | {} | {} | {} | {} |".format(b[1][4],b[2][4],b[3][4],b[4][4],b[5][4]))
-        print("  |-------------------|")
-        print("3 | {} | {} | {} | {} | {} |".format(b[1][3],b[2][3],b[3][3],b[4][3],b[5][3]))
-        print("  |-------------------|")
-        print("2 | {} | {} | {} | {} | {} |".format(b[1][2],b[2][2],b[3][2],b[4][2],b[5][2]))
-        print("  |-------------------|")
-        print("1 | {} | {} | {} | {} | {} |".format(b[1][1],b[2][1],b[3][1],b[4][1],b[5][1]))
-        print("  |-------------------|")
-        print("  |||||||||||||||||||||")
-        print("  |-------------------|")  
-        print("5 | {} | {} | {} | {} | {} |     ".format(a[1][5],a[2][5],a[3][5],a[4][5],a[5][5]))
-        print("  |-------------------|          ")
-        print("4 | {} | {} | {} | {} | {} |     ".format(a[1][4],a[2][4],a[3][4],a[4][4],a[5][4]))
-        print("  |-------------------|          ")
-        print("3 | {} | {} | {} | {} | {} |     ".format(a[1][3],a[2][3],a[3][3],a[4][3],a[5][3]))
-        print("  |-------------------|          ")
-        print("2 | {} | {} | {} | {} | {} |     Small Battleship".format(a[1][2],a[2][2],a[3][2],a[4][2],a[5][2]))
-        print("  |-------------------|     O")
-        print("1 | {} | {} | {} | {} | {} |".format(a[1][1],a[2][1],a[3][1],a[4][1],a[5][1]))
-        print("   ------------------- ")
-        print("    A   B   C   D   E")
-    else:
-        print("    A   B   C   D   E")
-        print("   ------------------- ")  
-        print("5 | {} | {} | {} | {} | {} |".format(b[1][5],b[2][5],b[3][5],b[4][5],b[5][5]))
-        print("  |-------------------|")
-        print("4 | {} | {} | {} | {} | {} |".format(b[1][4],b[2][4],b[3][4],b[4][4],b[5][4]))
-        print("  |-------------------|")
-        print("3 | {} | {} | {} | {} | {} |".format(b[1][3],b[2][3],b[3][3],b[4][3],b[5][3]))
-        print("  |-------------------|")
-        print("2 | {} | {} | {} | {} | {} |".format(b[1][2],b[2][2],b[3][2],b[4][2],b[5][2]))
-        print("  |-------------------|")
-        print("1 | {} | {} | {} | {} | {} |".format(b[1][1],b[2][1],b[3][1],b[4][1],b[5][1]))
-        print("  |-------------------|")
-        print("  |||||||||||||||||||||")
-        print("  |-------------------|")  
-        print("5 | {} | {} | {} | {} | {} |     ".format(a[1][5],a[2][5],a[3][5],a[4][5],a[5][5]))
-        print("  |-------------------|          ")
-        print("4 | {} | {} | {} | {} | {} |     ".format(a[1][4],a[2][4],a[3][4],a[4][4],a[5][4]))
-        print("  |-------------------|          ")
-        print("3 | {} | {} | {} | {} | {} |     ".format(a[1][3],a[2][3],a[3][3],a[4][3],a[5][3]))
-        print("  |-------------------|          ")
-        print("2 | {} | {} | {} | {} | {} |     ".format(a[1][2],a[2][2],a[3][2],a[4][2],a[5][2]))
-        print("  |-------------------|          ")
-        print("1 | {} | {} | {} | {} | {} |".format(a[1][1],a[2][1],a[3][1],a[4][1],a[5][1]))
-        print("   ------------------- ")
-        print("    A   B   C   D   E")
-
+from os import system, name 
 import random 
+
+# clear screen
+def clear_output(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
+def display_board(a,b):
+
+        print("   AI's Ocean\n")  
+        print("    A   B   C   D   E            AI's Ships")
+        print("   -------------------           -------------------")  
+        print("5 | {} | {} | {} | {} | {} |          Battleship".format(b[1][5],b[2][5],b[3][5],b[4][5],b[5][5]))
+        print("  |-------------------|          O O O")
+        print("4 | {} | {} | {} | {} | {} |          Cruiser 1".format(b[1][4],b[2][4],b[3][4],b[4][4],b[5][4]))
+        print("  |-------------------|          O O")
+        print("3 | {} | {} | {} | {} | {} |          Cruiser 2".format(b[1][3],b[2][3],b[3][3],b[4][3],b[5][3]))
+        print("  |-------------------|          O O")
+        print("2 | {} | {} | {} | {} | {} |          Submarine".format(b[1][2],b[2][2],b[3][2],b[4][2],b[5][2]))
+        print("  |-------------------|          O")
+        print("1 | {} | {} | {} | {} | {} |".format(b[1][1],b[2][1],b[3][1],b[4][1],b[5][1]))
+        print("  |-------------------|")
+        print("  |||||||||||||||||||||          Player's Ships")
+        print("  |-------------------|          -------------------")  
+        print("5 | {} | {} | {} | {} | {} |          Battleship".format(a[1][5],a[2][5],a[3][5],a[4][5],a[5][5]))
+        print("  |-------------------|          O O O")
+        print("4 | {} | {} | {} | {} | {} |          Cruiser 1".format(a[1][4],a[2][4],a[3][4],a[4][4],a[5][4]))
+        print("  |-------------------|          O O")
+        print("3 | {} | {} | {} | {} | {} |          Cruiser 2".format(a[1][3],a[2][3],a[3][3],a[4][3],a[5][3]))
+        print("  |-------------------|          O O")
+        print("2 | {} | {} | {} | {} | {} |          Submarine".format(a[1][2],a[2][2],a[3][2],a[4][2],a[5][2]))
+        print("  |-------------------|          O")
+        print("1 | {} | {} | {} | {} | {} |".format(a[1][1],a[2][1],a[3][1],a[4][1],a[5][1]))
+        print("   ------------------- ")
+        print("    A   B   C   D   E\n")
+        print("   Player's Ocean\n")
 
 horizontal_or_vertical = ['h','v']
 
 ai_x_coor = {1:'A',2:'B',3:'C',4:'D',5:'E'}
-
 x_coor = {'A':1,'B':2,'C':3,'D':4,'E':5}
 
 coordinates = [0] + [[x for x in [0,'1','2','3','4','5']] for x in [0,'A','B','C','D','E']]
 
 mygrid = [0] + [[0] + [' ' for x in range(5)] for x in range(5)]
-
 aigrid = [0] + [[0] + [' ' for x in range(5)] for x in range(5)]
-
-def check_vertical_placement(largeship):
-    
-    while True:
-        if int(largeship[0][1]) + 1 == int(largeship[1][1]) and int(largeship[1][1]) + 1 == int(largeship[2][1]):
-            print("This works.")
-            break
-        else:
-            print("These coordinates are not next to each other.")
-            largeship = []
-            lship1 = 1
-            continue
-
-def check_horizontal_placement(largeship):
-    
-    while True:
-        if x_coor[largeship[0][0].upper()] + 1 == x_coor[largeship[1][0].upper()] and x_coor[largeship[1][0].upper()]  + 1 == x_coor[largeship[2][0].upper()]:
-            print("This works.")
-            break
-        else:
-            print("These coordinates are not next to each other.")
-            largeship = []
-            lship1 = 1
-            continue
+hiddengrid = [0] + [[0] + [' ' for x in range(5)] for x in range(5)]
 
 intro = True
 game_start = True
@@ -179,6 +62,7 @@ while game_start:
     while intro:
         print("\nWelcome to Battleship!") 
         print("Place your battleships onto the board.")
+        x = input("Press Enter to Continue: ")
         
         place_largeship = True
         break
@@ -193,6 +77,8 @@ while game_start:
         # ask the player to input 3 coordinates for the large battleship and then break
         while len(largeship) < 3:
             
+            clear_output()
+            display_board(mygrid,hiddengrid)
             x = str(input("Enter coordinate " + str(counter) + " for the large ship:"))
 
             if len(x) != 2:
@@ -254,6 +140,8 @@ while game_start:
                     
         while len(mediumship1) < 2:
 
+            clear_output()
+            display_board(mygrid,hiddengrid)
             x = str(input("Enter coordinate " + str(counter) + " for the first medium ship:"))
 
             if len(x) != 2:
@@ -313,6 +201,8 @@ while game_start:
                     
         while len(mediumship2) < 2:
 
+            clear_output()
+            display_board(mygrid,hiddengrid)
             x = str(input("Enter coordinate " + str(counter) + " for the second medium ship:"))
 
             if len(x) != 2:
@@ -367,6 +257,8 @@ while game_start:
         
         while len(smallship) < 1:
 
+            clear_output()
+            display_board(mygrid,hiddengrid)
             x = str(input("Enter coordinate " + str(counter) + " for the small ship:"))
 
             if len(x) != 2:
@@ -464,6 +356,7 @@ while game_start:
             x = random.choice(['A','B','C','D','E'])
             y = random.choice([1,2,3])  
 
+
             ai_mediumship1.append(x + str(y))
             ai_mediumship1.append(x + str(y+1))
             
@@ -471,8 +364,8 @@ while game_start:
                 aigrid[x_coor[x]][y] = 'O'
                 aigrid[x_coor[x]][y+1] = 'O'
                 display_board(mygrid, aigrid)
-                place_mediumship1 = False
-                place_mediumship2 = True
+                ai_place_mediumship1 = False
+                ai_place_mediumship2 = True
                 break
                 
             else:
@@ -563,3 +456,7 @@ while game_start:
                 
             else:
                 continue
+    
+    display_board(mygrid, aigrid)
+    print(mygrid)
+    print(aigrid)
